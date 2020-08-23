963068b006e0555a865757fc2451a0c4bde58fba
Luc van Kerkvoort <luc.van.kerkvoort@gmail.com>
2020-08-22 18:14:35 -0700

    Updates

---
+++ precache-manifest.f732f85e11a09edfd5d16da80c240696.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6fb88158aa44709107fc3347bd31e95",
+    "url": "/locals/index.html"
+  },
+  {
+    "revision": "1dee55d9de1b0ebae003",
+    "url": "/locals/static/css/main.e5502048.chunk.css"
+  },
+  {
+    "revision": "4a4630b8b6ea8169cda6",
+    "url": "/locals/static/js/2.9170e1ac.chunk.js"
+  },
+  {
+    "revision": "3c169b3504107d5b8af6dc344997c66e",
+    "url": "/locals/static/js/2.9170e1ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1dee55d9de1b0ebae003",
+    "url": "/locals/static/js/main.2d6921a6.chunk.js"
+  },
+  {
+    "revision": "a6692e92e6d4de7f5d55",
+    "url": "/locals/static/js/runtime-main.23ba090b.js"
+  },
+  {
+    "revision": "546249d434bc708ff33e1855d981c1b0",
+    "url": "/locals/static/media/Locals.546249d4.png"
+  },
+  {
+    "revision": "bcba3a51adff6555b8ef5cc9710fd037",
+    "url": "/locals/static/media/jumbotron1.bcba3a51.jpg"
+  }
+]);
